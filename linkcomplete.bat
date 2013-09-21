c74e2945f027eb890833a6e7616cb42b786c5f29
Jeff Mu <smartmu@gmail.com>
2013-09-21 15:55:27 -0400

    added check if torrent is directory

--- linkcomplete.bat
+++ linkcomplete.bat
@@ -1,11 +1,20 @@
 @ECHO OFF
-set TORRENT_ID = %1
-set TORRENT_NAME = %2
-set SAVE_DIR = %3
+set TORRENT_ID=%1
+set TORRENT_NAME=%2
+set SAVE_DIR=%3
 
 pushd %SAVE_DIR%
 cd ..
-set TEMP_DIR = %cd%
-popd
+set TEMP_DIR=%cd%
 
-mklink /h %TEMP_DIR% %SAVE_DIR%\%TORRENT_NAME%
+if exist %TORRENT_NAME%\ (
+    set DEST_DIR=%TEMP_DIR%\#temp\%TORRENT_NAME%
+    mkdir %DEST_DIR%
+    pushd %SAVE_DIR%\%TORRENT_NAME%
+    forfiles /C "cmd /c mklink /h %DEST_DIR%\@file @file"
+) else (
+    set DEST_DIR=%TEMP_DIR%\#temp\
+    mklink /h %DEST_DIR%\%TORRENT_NAME% %SAVE_DIR%\%TORRENT_NAME%
+)
+
+
