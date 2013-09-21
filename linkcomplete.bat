394b7071c35064769accbf5d92cedc11ee893861
Jeff Mu <smartmu@gmail.com>
2013-09-21 16:49:09 -0400

    cleanup and fixes

--- linkcomplete.bat
+++ linkcomplete.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
+SETLOCAL
+
 set TORRENT_ID=%1
 set TORRENT_NAME=%2
 set SAVE_DIR=%3
@@ -6,15 +8,17 @@ set SAVE_DIR=%3
 pushd %SAVE_DIR%
 cd ..
 set TEMP_DIR=%cd%
+set DEST_DIR=%TEMP_DIR%\#temp
 
-if exist %TORRENT_NAME%\ (
-    set DEST_DIR=%TEMP_DIR%\#temp\%TORRENT_NAME%
-    mkdir %DEST_DIR%
+if exist %SAVE_DIR%\%TORRENT_NAME%\ (
+    if not exist %DEST_DIR%\%TORRENT_NAME% mkdir %DEST_DIR%\%TORRENT_NAME%
     pushd %SAVE_DIR%\%TORRENT_NAME%
-    forfiles /C "cmd /c mklink /h %DEST_DIR%\@file @file"
+    forfiles /C "cmd /c mklink /h %DEST_DIR%\%TORRENT_NAME%\@file @file"
 ) else (
-    set DEST_DIR=%TEMP_DIR%\#temp\
+    if not exist %DEST_DIR% mkdir %DEST_DIR%
     mklink /h %DEST_DIR%\%TORRENT_NAME% %SAVE_DIR%\%TORRENT_NAME%
 )
 
+pushd %SAVE_DIR%
 
+ENDLOCAL
