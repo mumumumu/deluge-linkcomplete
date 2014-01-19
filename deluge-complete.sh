02d4ca03c4fa60a400ca09cf3de46e03bf0dd089
jeff <jeff@mux4.com>
2014-01-18 21:08:14 -0500

    migrated htpc to ubuntu, so created a simple bash script for linking completed torrents

---
+++ deluge-complete.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# create a hard link upon torrent completion from seeding directory to a temporary directory for post-processing
+
+torrent_id=$1
+torrent_name=$2
+torrent_path=$3
+dest_path=${torrent_path/seeding/temp}
+
+cp -Rlv $torrent_path/$torrent_name $dest_path >> /tmp/deluge_execute.log
