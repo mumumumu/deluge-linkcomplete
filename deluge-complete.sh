31fb5d2fd6cd1d2544abe8070e6ee0eee2ca61bf
jeff <jeff@mux4.com>
2014-01-26 11:16:35 -0500

    updates
    - log actions with time stamp
    - handle filenames with spaces in them

--- deluge-complete.sh
+++ deluge-complete.sh
@@ -6,4 +6,9 @@ torrent_name=$2
 torrent_path=$3
 dest_path=${torrent_path/seeding/temp}
 
-cp -Rlv $torrent_path/$torrent_name $dest_path >> /tmp/deluge_execute.log
+timestamp() {
+    date
+}
+
+echo -n [$(timestamp)] >> /tmp/deluge_execute.log
+cp -Rlv "$torrent_path/$torrent_name" "$dest_path" >> /tmp/deluge_execute.log
